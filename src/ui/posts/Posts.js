8223c3800734a882d3544fbc124256affcd58207
Komal Chaudhary <86321540+kchaudhary1@users.noreply.github.com>
2021-09-10 15:37:13 -0600

    Initialize dependabot/npm_and_yarn/y18n-4.0.1

---
+++ src/ui/posts/Posts.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import { PostCard } from '../shared/PostCard'
+import { fetchAllPostsAndUsers } from '../../store/posts'
+
+
+export const Posts = () => {
+
+	const dispatch = useDispatch();
+
+	console.log(dispatch)
+
+	const effects = () => {
+		dispatch(fetchAllPostsAndUsers());
+	};
+
+	const inputs = [];
+
+	useEffect(effects, inputs);
+
+	const posts = useSelector(state => (state.posts ? state.posts : []));
+
+
+
+	return (
+		<>
+			<div className="card-columns">
+				{posts.map(post => <PostCard post={post} key={post.postId}/>)}
+			</div>
+		</>
+	);
+
+
+};
+
